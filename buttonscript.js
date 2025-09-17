d23864617d2f0dc85ea343f942af05a3c55d0f47
Ben Givens <ben.s.givens@gmail.com>
2025-09-16 18:27:13 -0700

    cleaned it up

--- buttonscript.js
+++ buttonscript.js
@@ -4,13 +4,13 @@ function resume()
     window.open('benGivensResume.pdf', '_blank');
 }
 function battleShipButton(){
-    window.location.href ='https://github.com/ryebsgiv/BattleShip';
+    window.location.href ='https://github.com/bsgive/BattleShip';
 }
 function parserButton(){
-    window.location.href ='https://github.com/ryebsgiv/String-Parser';
+    window.location.href ='https://github.com/bsgive/String-Parser';
 }
 function tailButton(){
-    window.location.href ='https://github.com/ryebsgiv/Tail-function';
+    window.location.href ='https://github.com/bsgive/Tail-function';
 }
 function navigateToSection(sectionId) {
     var section = document.getElementById(sectionId);
